1e4bf14c3e01d476e2d85ff8b5e020929f9a189b
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-06-29 13:46:22 +1000

    context switch

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import find_packages, setup
 import os
 
 setup(
-    name='pyisva',
+    name='pyisam',
     version='0.1.%s' % os.environ.get('TRAVIS_BUILD_NUMBER', 0),
     description='Python API for IBM Security Verify Access',
     author='Lachlan Gleeson',
     author_email='lgleeson@au1.ibm.com',
     license='MIT',
-    package_dir={"": "pyisva"},
-    packages=find_packages(where="pyisva"),
+    package_dir={"": "pyisam"},
+    packages=find_packages(where="pyisam"),
     install_requires=[
         'requests>=2.23.0'
     ],
