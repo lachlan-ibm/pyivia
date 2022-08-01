62c1beaab31948ed36f2b4d77aa9ffda2b1a22b8
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-02 08:05:06 +1000

    syntax error

--- .tests/unit_test.sh
+++ .tests/unit_test.sh
@@ -2,8 +2,8 @@
 python setup.py sdist bdist_wheel
 export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
 python <<EOF
-import pyisva
-f = pyisva.factory("https://ibm.security.verify.access", "user", "password")
+from pyisva.factory import Factory
+f = Factory("https://ibm.security.verify.access", "user", "password")
 EOF
 
 exit 0
