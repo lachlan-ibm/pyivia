e44afe388617bd3b0d2e36e2bc00605d534e4f3c
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-02 08:58:38 +1000

    Update build to mock http server for test import

--- .tests/unit_test.sh
+++ .tests/unit_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 python setup.py sdist bdist_wheel
 export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
-cd python -m http.server &
+cd .tests && python -m http.server &
 SERVER_PID="$!"
 python <<EOF
 import pyisva
-f = pyisva.Factory("http://localhost", "user", "password")
+f = pyisva.Factory("http://localhost:8000", "user", "password")
 EOF
 kill $SERVER_PID
 exit 0
