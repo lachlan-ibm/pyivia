fb6ae9df731c72e612069d7ab3c8a2f3fdf8b4b5
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-02 07:49:00 +1000

    ADD MIT licesne

--- .tests/unit_test.sh
+++ .tests/unit_test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+python setyp.py sdist bdist_wheel
+export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
+python <<EOF
+import pyisva
+f = pyisva.factory("https://ibm.security.verify.access", "user", "password")
+EOF
 
 exit 0
