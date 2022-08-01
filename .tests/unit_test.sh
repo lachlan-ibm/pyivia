a523b512ac22c8cad8d1b9f3a85a78d4d3ebbb6e
Lachlan Gleeson <lgleeson@au1.ibm.com>
2022-08-02 08:46:46 +1000

    Update build to mock http server for test import

--- .tests/unit_test.sh
+++ .tests/unit_test.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 python setup.py sdist bdist_wheel
 export PYTHONPATH="$PYTHONPATH:$(pwd)/build/lib"
+cd python -m http.server &
+SERVER_PID="$!"
 python <<EOF
-from pyisva.factory import Factory
-f = Factory("https://ibm.security.verify.access", "user", "password")
+import pyisva
+f = pyisva.Factory("http://localhost", "user", "password")
 EOF
-
+kill $SERVER_PID
 exit 0
