a88c85560a430bee6ddecd1ad018e97651d4b6e2
Lachlan Gleeson <lgleeson@au1.ibm.com>
2023-04-03 17:24:21 +1000

    Add missing version

--- README.md
+++ README.md
@@ -13,6 +13,9 @@ quick and easy way to construct configuration scripts for appliances.
 - IBM Security Verify Access 10.0.2.0
 - IBM Security Verify Access 10.0.1.0
 - IBM Security Verify Access 10.0.0.0
+- IBM Security Access Manager 9.0.7.3
+- IBM Security Access Manager 9.0.7.2
+- IBM Security Access Manager 9.0.7.1
 - IBM Security Access Manager 9.0.7.0
 - IBM Security Access Manager 9.0.6.0
 - IBM Security Access Manager 9.0.5.0
--- pyisam/factory.py
+++ pyisam/factory.py
@@ -9,7 +9,7 @@ from pyisam.util.restclient import RESTClient
 
 DEVELOPMENT_VERSION = "IBM Security Access Manager Development"
 VERSIONS = {
-    DEVELOPMENT_VERSION: "10050",
+    DEVELOPMENT_VERSION: "10060",
     "IBM Security Verify Access 10.0.6.0_b1": "10060",
     "IBM Security Verify Access 10.0.6.0": "10060",
     "IBM Security Verify Access 10.0.5.0": "10050",
@@ -23,6 +23,8 @@ VERSIONS = {
     "IBM Security Access Manager 10.0.0.0_b1": "10000",
     "IBM Security Access Manager 10.0.0.0": "10000",
     "IBM Security Access Manager 9.0.8.0": "9080",
+    "IBM Security Access Manager 9.0.7.3": "9073",
+    "IBM Security Access Manager 9.0.7.2": "9072",
     "IBM Security Access Manager 9.0.7.1": "9071",
     "IBM Security Access Manager 9.0.7.0": "9070",
     "IBM Security Access Manager 9.0.6.0_b2": "9060",
