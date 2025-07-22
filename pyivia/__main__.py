ffb1257515f6628ab6119d7c0cd20069b1774f2e
Peter T. Cogill <pcogill@au1.ibm.com>
2025-07-22 15:04:13 +1000

    Main entrypoint updates

--- pyivia/__main__.py
+++ pyivia/__main__.py
@@ -1,40 +1,86 @@
 #!/usr/bin/env python3
 
-
 import argparse
 import logging
-import os
 import pyivia
 import inspect
+import json
+import getpass
 
-logger = logging.getLogger(__name__)
-
+logger = logging.getLogger("pyivia")
+cli_config = { 
+    'json_format' : True
+}
 
 def get_functions(obj):
-    type_dict = type(obj).__dict__
-
+    r"""
+    Get functions from the obj that can be called via the CLI
+    """
     results = { }
 
-    for name, value in type_dict.items():
-        if not name.startswith("_") and callable(value):
-            results[name] = (value, inspect.signature(value))
+    for obj_type in type(obj).__mro__:
+        if obj_type == object:
+            continue
+
+        type_dict = obj_type.__dict__
+
+        for name, value in type_dict.items():
+            if not name.startswith("_") and callable(value):
+                if name not in results:
+                    results[name] = (value, inspect.signature(value))
+    
     return results
 
-def get_attributes(obj):
+def is_usable_attribute(name, value):
+    r"""
+    Is the attribute something we want to use in the CLI
+    """
+    if name.startswith("_"):
+        # Don't use 'private' variables
+        return False
+    elif value is None:
+        # Can't do much with null values
+        return False
+    elif type(value) == str or type(value) == pyivia.util.restclient.RESTClient:
+        # Don't try to work with these value types
+        return False
+    else:
+        # Everything else is fine to use
+        return True
+    
 
+def get_attributes(obj):
+    r"""
+    Get attributes from the obj that can be accessed via the CLI
+    """
     attrs = { }
     for name,value in obj.__dict__.items():
-        if not name.startswith("_") and value is not None and type(value) != str:
+        if is_usable_attribute(name, value):
             attrs[name] = value
+
     return attrs
 
+def is_blank(value):
+    r"""
+    Is the value blank
+    """
+    return value is None or len(value) == 0 or value.isspace()
+
 def parse_value(value):
-    logger.debug(f"Parsing value: {value}")
-    if value.startswith("[") and value.endswith("]"):
-        value = eval(value)
-    return value
+    r"""
+    Parse a value provided on the command line into an appropriate type.
+    """
+    try:
+        # This should handle all data types except string
+        return json.loads(value)
+    except:
+        # It's a string
+        return value
 
 def parse_param(param):
+    r"""
+    Parse a parameter into a name (if specified) and a value.
+    """
     name = None
     value = None
 
@@ -45,12 +91,13 @@ def parse_param(param):
     else:
         value = param
 
-    value = parse_value(value)
-    return name,value
-
+    return name,parse_value(value)
 
 
 def parse_args(object, signature, remaining_params):
+    r"""
+    Parse the remaining command line arguments into function parameters.
+    """
     args = [object]
     kwargs = { }
 
@@ -58,16 +105,17 @@ def parse_args(object, signature, remaining_params):
     named_params = {}
 
     for param in remaining_params:
-        if param is None or len(param) == 0 or param.isspace():
+        if is_blank(param):
             continue
 
         param_name, param_value = parse_param(param)
         if param_name in signature.parameters:
             named_params[param_name] = param_value
         else:
+            # Not a known parameter, assume it's just a value with an equals sign in it.
             unnamed_params.append(parse_value(param))
 
-    for name,value in signature.parameters.items():
+    for name in signature.parameters.keys():
         if name == "self":
             continue
 
@@ -79,22 +127,82 @@ def parse_args(object, signature, remaining_params):
                 unnamed_params = unnamed_params[1:]
             else:
                 logger.debug(f"Cannot find something to bind to {name}")
+
     return signature.bind(*args, **kwargs)
+
+def is_response(obj):
+    r"""
+    Does the object represent a response that should be displayed?
+    """
+    return type(obj) == pyivia.util.model.Response or \
+        type(obj) == bool or type(obj) == str
+
+def display_response(result):
+    r"""
+    Display the response.
+    """
+    if (type(result) == pyivia.util.model.Response):
+        print(f"Status: {result.status_code}")
+        if result.json is not None:
+            if cli_config['json_format']:
+                print(json.dumps(result.json, indent=4))
+            else:
+                print(result.json)
+    else:
+        print(result)
+
+def print_signature(signature):
+    r"""
+    Print the method signature in a manner that is more relevant to the CLI
+    """
+    signature_str = str(signature)
+    self_prefix = "(self, "
+    if signature_str == "(self)":
+        print("()")
+    elif signature_str.startswith(self_prefix):
+        print("(" + signature_str[len(self_prefix):])
+
 
 
 def run_command(object, method, signature, remaining_params):
-    try:
-        bound_args = parse_args(object, signature, remaining_params)
-        result = method(*bound_args.args, **bound_args.kwargs)
-        print(f"{result}")
-    except:
-        print(f"Did not get required parameters: {signature}")
+    r"""
+    Run a command and print the result
+    """
+    if len(remaining_params) > 0 and remaining_params[-1] == "-?":
+        print_signature(signature)
+    else:
+        try:
+            bound_args = parse_args(object, signature, remaining_params)
+            result = method(*bound_args.args, **bound_args.kwargs)
+            if is_response(result):
+                display_response(result)
+            else:
+                print(result)
+        except Exception as e:
+            print(f"{e}")
+    
 
-parser = argparse.ArgumentParser()
+def print_cmd_usage(cmd):
+    r"""
+    Print the usage for a command, showing what can be done with it
+    """
+    funcs = get_functions(cmd)
+    attrs = get_attributes(cmd)
+    all_options = sorted(list(funcs.keys()) + list(attrs.keys()))
+
+    print("Possible options are:")
+    print("    " + "\n    ".join(all_options))
+
+
+# The main line
+
+parser = argparse.ArgumentParser("pyivia")
 parser.add_argument("-v", "--verbose", help="Enable verbose logging", action="store_true", default=False)
 parser.add_argument("-b", "--base_url", help="IVIA Base URL", required=True)
 parser.add_argument("-u", "--username", default="admin", help="Username")
-parser.add_argument("-p", "--password", default="admin", help="Password")
+parser.add_argument("-p", "--password", default=None, help="Password")
+parser.add_argument("--json-format", default=True, dest="json_format", action="store_true", help="Apply formatting to JSON output.")
+parser.add_argument("--no-json-format", dest="json_format", action="store_false", help="Do not apply formatting to JSON output.")
 parser.add_argument("command", nargs=argparse.REMAINDER, help="Command")
 
 args = parser.parse_args()
@@ -104,43 +212,55 @@ if args.verbose:
 else:
     logging.basicConfig(level=logging.INFO)
 
-appliance = pyivia.Factory(args.base_url, args.username, args.password)
-cmd_stack = [appliance]
+password = args.password
+if password is None:
+    password = getpass.getpass()
+
+appliance = pyivia.Factory(args.base_url, args.username, password)
+current_obj = appliance
 
 ran_cmd = False
 
+cli_config['json_format'] = args.json_format
+
 for idx,cmd in enumerate(args.command):
     logger.debug(f"Processing command [{cmd}]")
-    current = cmd_stack[-1]
+    if is_blank(cmd):
+        continue
 
-    if not callable(current):
-        functions = get_functions(current)
+    if not callable(current_obj):
+        functions = get_functions(current_obj)
         func, sig = None, None
         for name in [cmd, f"get_{cmd}" ]:
             if name in functions:
                 (func, sig) = functions[name]
                 break
-
+        
         if func is not None:
             if len(sig.parameters) == 1:
-                cmd_stack.append(func(current))
+                result = func(current_obj)
+                if is_response(result):
+                    display_response(result)
+                    ran_cmd = True
+                    break
+                else:
+                    current_obj = func(current_obj)
             else:
-                run_command(current, func, sig, args.command[idx+1:])
+                run_command(current_obj, func, sig, args.command[idx+1:])
                 ran_cmd = True
                 break
-                break
+
         else:
-            attrs = get_attributes(current)
+            attrs = get_attributes(current_obj)
             if cmd in attrs:
-                cmd_stack.append(attrs[cmd])
+                current_obj = attrs[cmd]
+            elif cmd == "-?":
+                print_cmd_usage(current_obj)
+                ran_cmd = True
+                break
             else:
                 logger.error(f"Could not find how to handle {cmd}")
                 break
 
 if not ran_cmd:
-    funcs = get_functions(cmd_stack[-1])
-    attrs = get_attributes(cmd_stack[-1])
-    all_options = sorted(list(funcs.keys()) + list(attrs.keys()))
-    all_options_str = "\n    ".join(all_options)
-    print("Could not find a command to run. Possible options are:")
-    print(f"    {all_options_str}")
+    print_cmd_usage(current_obj)
